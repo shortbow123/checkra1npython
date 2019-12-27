7cd04dc0bdea21c20042728b9d942d1d7a9c4bcc
me <nope>
2019-12-26 17:59:25 -0800

    Fixing and lableing setup file
    Labeling stuff for debugging and information purposes. Also fixing cloned github directory and service installation

--- setup.sh
+++ setup.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-#echo Please enter your username:
-#read username
-cd $HOME/checkra1npython
+echo Please enter your username:
+read username
+cd $username/checkra1npython
+echo Downloading latest checkra1npython version
 wget https://raw.githubusercontent.com/shortbow123/checkra1npython/master/program.py
-sudo cp -rf $HOME/checkra1npython/checkra1npythongui.service /lib/systemd/system/checkra1npythongui.service
+echo Copying service from github clone setup files to system services location
+sudo cp -rf $username/checkra1npython/checkra1npythongui.service /lib/systemd/system/checkra1npythongui.service
+echo running chmod on service
 sudo chmod 644 /lib/systemd/system/checkra1npythongui.service
+echo restarting systemctl daemon
 sudo systemctl daemon-reload
+echo enabling checkra1npythongui.service
 sudo systemctl enable checkra1npythongui.service
 echo This device needs to restart. Press enter now or CTRL + C to stop this setup and restart manually later. Install will finalize on next reboot.
 read restartvarr
