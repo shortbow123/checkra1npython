901036ada8af49d3a1fb8df3700a4829423e1b5a
me <nope>
2020-02-02 09:28:13 -0800

    merge with private git server

--- checkra1npythongui/program.py
+++ checkra1npythongui/program.py
@@ -2,8 +2,6 @@ import os
 import pygame as pg
 import wget
 import time
-#To be used soon
-#import keyboard
 
 #add config.txt for font saving and all settings
 version = "2"
@@ -22,7 +20,7 @@ try:
 except FileNotFoundError:
     print("No old update files to delete.")
 
-try: 
+try:
     #Check for dev mode, if no devmode file is present, put it into release mode
     devFileCheck = open("devmode")
     devLines = devFileCheck.readlines()
@@ -33,11 +31,12 @@ try:
     else:
         devmode = False
         print("Release Mode")
-    
-    
+
+
+
 except FileNotFoundError:
     print("Release Mode")
-    
+
 #checks for the dev term
 if devKeyword == "developer":
     devmode = True
@@ -45,7 +44,7 @@ if devKeyword == "developer":
 else:
     devmode = False
     print("Release Mode")
-    
+
 #Enabled until it is working correctly on my rpi :(
 devmode = True
 
@@ -63,7 +62,7 @@ if devmode == False:
         print("An update is available.")
         print("Starting update now...")
         os.system('python3 update.py')
-        
+
 #Again, try to delete le files
 try:
     os.remove("availableVersion.py")
@@ -72,18 +71,18 @@ except FileNotFoundError:
     print("No update files to delete.")
 
 #Make sure the display is inizialized, if not initialize it
-if pg.display.get_init() != 1: 
+if pg.display.get_init() != 1:
     pg.display.init()
     print("display initialized")
 
 #Checks for custom screen dimensions, if there is no custom dimentions, falls back to default.
-try: 
+try:
     customScreen = open("resources/settings/CustomDimensions")
     customScreenLines = customScreen.readlines()
     customScreenWidth = customScreenLines[0]
     customScreenHeight = customScreenLines[1]
     (w, h) = (customScreenWidth, customScreenHeight)
-    
+
 except FileNotFoundError:
     print("No custom screen dimensions, falling back to default")
     (w, h) = (720, 480)
@@ -115,18 +114,19 @@ def homeScreen():
     global button4
     global button5
     global settingsButton
+
     screen.fill(black)
     curScreen = 0
-    
-    #Give dimensions and locations of the buttons
-    checkra1nButton = pg.Rect(35, 35, 200, 200)
-    rcmButton = pg.Rect(35, 260, 200, 200)
-    button3 = pg.Rect(260, 36, 200, 200)
-    button4 = pg.Rect(260, 260, 200, 200)
-    button5 = pg.Rect(485, 37, 200, 200)
-    settingsButton = pg.Rect(485, 260, 200, 200)
 
-    
+    #Give dimensions and locations of the buttons
+    checkra1nButton = pg.Rect(35, 25, 200, 200)
+    rcmButton = pg.Rect(35, 250, 200, 200)
+    button3 = pg.Rect(260, 26, 200, 200)
+    button4 = pg.Rect(260, 250, 200, 200)
+    button5 = pg.Rect(485, 27, 200, 200)
+    settingsButton = pg.Rect(485, 250, 200, 200)
+
+
     #Give text to the buttons
     checkra1nText = mainFont.render("checkra1n test", True, white)
     rcmText = mainFont.render("RCM payloads", True, white)
@@ -134,62 +134,143 @@ def homeScreen():
     text4 = mainFont.render("4th Option", False, white)
     text5 = mainFont.render("5th Option", False, white)
     settingsText = mainFont.render("Settings", False, white)
-    
+
     print("here")
+    #print(curScreen)
     pg.draw.rect(screen, darkGrey, checkra1nButton)
     pg.draw.rect(screen, darkGrey, rcmButton)
     pg.draw.rect(screen, darkGrey, button3)
     pg.draw.rect(screen, darkGrey, button4)
     pg.draw.rect(screen, darkGrey, button5)
     pg.draw.rect(screen, darkGrey, settingsButton)
-    
+
     #put the text + images on the stuff
-    screen.blit(checkra1nText, (57, 125))
-    screen.blit(rcmText, (70, 350))
-    screen.blit(text3, (300, 125))
-    screen.blit(text4, (300, 350))
-    screen.blit(text5, (525, 125))
-    screen.blit(settingsText, (540, 350))
-    
+    screen.blit(checkra1nText, (57, 115))
+    screen.blit(rcmText, (70, 340))
+    screen.blit(text3, (300, 115))
+    screen.blit(text4, (300, 340))
+    screen.blit(text5, (525, 115))
+    screen.blit(settingsText, (540, 340))
+
+    pg.display.flip()
+
+
+def checkra1nScreen():
+    global curScreen
+
+    print("switching to checkra1n screen")
+    curScreen = 1
+    screen.fill(black)
+
+    #define the rectangles
+    pwnButton = pg.Rect(35, 25, 450, 200)
+
+    #text
+    pwnText = mainFont.render("pwn ur i", False, white)
+
+    #draw
+    pg.draw.rect(screen, darkGrey, pwnButton)
+
+    #put texts up
+
+    screen.blit(pwnText, (200,200))
+
+
+
+    print(curScreen)
+    pg.display.flip()
+    time.sleep(0.1)
+    while curScreen == 1:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+                pg.quit()
+        mouseButton = pg.mouse.get_pressed()
+        if mouseButton > (0, 0, 0):
+            homeScreen()
+            time.sleep(0.1)
+
+def rcmLoaderScreen():
+    global curScreen
+
+    print("switching to rcm loader screen")
+    curScreen = 2
+    screen.fill(lightGrey)
+
+    print(curScreen)
+    pg.display.flip()
+    time.sleep(0.1)
+    while curScreen == 2:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+                pg.quit()
+        mouseButton = pg.mouse.get_pressed()
+        if mouseButton > (0, 0, 0):
+            homeScreen()
+            time.sleep(0.1)
+
+
+def settingsScreen():
+    global curScreen
+
+    print("switching to settings pane")
+    curScreen = 6
+    screen.fill(grey)
+
+    print(curScreen)
+    pg.display.flip()
+    time.sleep(0.1)
+    while curScreen == 6:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+                pg.quit()
+        mouseButton = pg.mouse.get_pressed()
+        if mouseButton > (0, 0, 0):
+            homeScreen()
+            time.sleep(0.1)
+
+
 homeScreen()
 running = True
 while running:
-    
-    #flips display to see changes
-    screen.fill(blue)
-    pg.display.flip()
 
     #stops the loop
     for event in pg.event.get():
         if event.type == pg.QUIT:
-            running = False 
+            running = False
             pg.quit()
     mousePosition = pg.mouse.get_pos()
     mouseButton = pg.mouse.get_pressed()
     #get_display active == false:
         #make it active
-    
+
+    #print(curScreen)
+
     if curScreen == 0 and checkra1nButton.collidepoint(mousePosition) and mouseButton > (0, 0, 0):
-        print("switching to checkra1n")
-        screen.fill(blue)
+        checkra1nScreen()
         time.sleep(0.1)
+
     elif curScreen == 0 and rcmButton.collidepoint(mousePosition) and mouseButton > (0, 0, 0):
-        print("switching to rcm loader")
-        screen.fill(lightGrey)
+        rcmLoaderScreen()
         time.sleep(0.1)
+
     elif curScreen == 0 and button3.collidepoint(mousePosition) and mouseButton > (0, 0, 0):
         print("switching to button3")
-        screen.fill(black)
+        screen.fill(white)
         time.sleep(0.1)
+
     elif curScreen == 0 and button4.collidepoint(mousePosition) and mouseButton > (0, 0, 0):
         print("switching to button4")
         screen.fill(black)
         time.sleep(0.1)
+
     elif curScreen == 0 and button5.collidepoint(mousePosition) and mouseButton > (0, 0, 0):
         print("switching to button5")
         screen.fill(black)
         time.sleep(0.1)
+
     elif curScreen == 0 and settingsButton.collidepoint(mousePosition) and mouseButton > (0, 0, 0):
-        print("switching to settings pane")
-        screen.fill(grey)
+        settingsScreen()
         time.sleep(0.1)
